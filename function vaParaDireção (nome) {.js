ff58ef072ce345000020b9253f45ee72b348f074
vitorhenrique314 <vitor.vieira.araujo@escola.pr.gov.br>
2026-09-25 11:54:12 -0300

    Add files via upload

---
+++ function vaParaDireção (nome) {.js
@@ -0,0 +1,6 @@
+function vaParaDireção (nome) {
+    return nome + ", não é autorizado o uso de celular durante a aula! Ainda assim insiste em usar. Então seu celular será recolhido.";
+}
+
+let mensagem = vaParaDireção("Vitor");
+console.log(mensagem);
